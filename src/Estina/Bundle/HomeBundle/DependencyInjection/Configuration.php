930909a794e743928b9f79595570a2fb8b310bfa
Paulius Podolskis <pp@estina.com>
2014-11-20 15:17:16 +0000

    Restoring root node, coz its required

--- src/Estina/Bundle/HomeBundle/DependencyInjection/Configuration.php
+++ src/Estina/Bundle/HomeBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,11 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        
+        $rootNode = $treeBuilder->root('estina_home');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
 
         return $treeBuilder;
     }
